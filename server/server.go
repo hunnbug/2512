34f526d7483cf4808745f6322e042a36a559177f
humbug <feldolshkolnik@gmail.com>
2025-04-07 02:06:09 +0300

    penis music

--- server/server.go
+++ server/server.go
@@ -40,10 +40,10 @@ func Start() {
 	//AboutListener
 	r.GET("/listeners/:id/about", handlers.AboutListener)
 	r.PUT("/listeners/:id/about/update/passport", handlers.UpdateListenersPassport)
-	r.PUT("/listeners/:id/about/update/education", handlers.UpdateListenersPassport)
+	r.PUT("/listeners/:id/about/update/education", handlers.UpdateListenersEducationListener)
 	r.PUT("/listeners/:id/about/update/placework", handlers.UpdateListenersPlaceWork)
-	r.PUT("/listeners/:id/about/update/programeducation", handlers.UpdateListenersPassport)
 	r.PUT("/listeners/:id/about/update/regaddress", handlers.UpdateListenersRegAddress)
+	r.PUT("/listeners/:id/about/update/programeducation", handlers.UpdateListenersPassport)
 
 	r.Run("localhost:8080")
 
